084cd4045d59860250270bd773f81f23fef4d1a6
JuanCastano <juanescastano76@gmail.com>
2026-02-15 11:43:43 -0500

    added img

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 
 function Header() {
-  const audioRef = useRef<HTMLAudioElement>(null);
-
-  const handlePlay = () => {
-    audioRef.current?.play();
-  };
-
   return (
     <header
       className="relative bg-cover bg-center h-[98dvh] w-dvw max-w-[700px]"
@@ -17,20 +11,6 @@ function Header() {
       <div className="h-full flex items-center justify-center m-auto flex-col gap-8 pb-16">
         <h2 className="font-old text-xl tracking-wide">MIS XV AÑOS</h2>
         <h1 className="font-invitacion text-8xl tracking-wide">Susana</h1>
-      </div>
-
-      {/* Botón música */}
-      <div className="fixed bottom-6 right-6 z-50">
-        <button
-          onClick={handlePlay}
-          className="bg-pink-500 text-white px-4 py-2 rounded-full shadow-lg"
-        >
-          🎵 Reproducir música
-        </button>
-
-        <audio ref={audioRef} loop>
-          <source src="/music/cancion.mp3" type="audio/mpeg" />
-        </audio>
       </div>
     </header>
   );
